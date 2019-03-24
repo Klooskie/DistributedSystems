630f240550be6660df33ba5c32a00d1b8b599fe9
jakub <kuba.koniecznyxx@gmail.com>
2019-03-24 14:14:12 +0100

    fix state sending and move channel initialization to DistributedMap class constructor

--- lab2/DistributedHashMap/src/main/java/DistributedMap.java
+++ lab2/DistributedHashMap/src/main/java/DistributedMap.java
@@ -1,7 +1,14 @@
 import org.jgroups.*;
+import org.jgroups.protocols.*;
+import org.jgroups.protocols.pbcast.GMS;
+import org.jgroups.protocols.pbcast.NAKACK2;
+import org.jgroups.protocols.pbcast.STABLE;
+import org.jgroups.protocols.pbcast.STATE;
+import org.jgroups.stack.ProtocolStack;
 import org.jgroups.util.Util;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,10 +19,34 @@ public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
     private final JChannel communicationChannel;
     private final Map<String, Integer> hashTable;
 
-    public DistributedMap(JChannel communicationChannel) {
-        this.communicationChannel = communicationChannel;
-        this.communicationChannel.setReceiver(this);
+    public DistributedMap() throws Exception {
         this.hashTable = new HashMap<>();
+
+        this.communicationChannel = new JChannel(false);
+        ProtocolStack stack = new ProtocolStack();
+        this.communicationChannel.setProtocolStack(stack);
+
+        stack.addProtocol(new UDP().setValue("mcast_group_addr", InetAddress.getByName("224.7.7.7")))
+                .addProtocol(new PING())
+                .addProtocol(new MERGE3())
+                .addProtocol(new FD_SOCK())
+                .addProtocol(new FD_ALL().setValue("timeout", 12000).setValue("interval", 3000))
+                .addProtocol(new VERIFY_SUSPECT())
+                .addProtocol(new BARRIER())
+                .addProtocol(new NAKACK2())
+                .addProtocol(new UNICAST3())
+                .addProtocol(new STABLE())
+                .addProtocol(new GMS())
+                .addProtocol(new UFC())
+                .addProtocol(new MFC())
+                .addProtocol(new FRAG2())
+                .addProtocol(new STATE());
+        stack.init();
+
+        this.communicationChannel.setReceiver(this);
+        this.communicationChannel.setDiscardOwnMessages(true);
+        this.communicationChannel.connect("distributed_map_cluster");
+        this.communicationChannel.getState(null, 0);
     }
 
     @Override
@@ -36,7 +67,7 @@ public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
 
     private void putNotify(String key, Integer value) {
         PutNotification notification = new PutNotification(key, value);
-        Message message = new Message(null, null, notification);
+        Message message = new Message(null, notification);
         try {
             this.communicationChannel.send(message);
         } catch (Exception e) {
@@ -53,7 +84,7 @@ public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
 
     private void removeNotify(String key) {
         RemovalNotification notification = new RemovalNotification(key);
-        Message message = new Message(null, null, notification);
+        Message message = new Message(null, notification);
         try {
             this.communicationChannel.send(message);
         } catch (Exception e) {
@@ -68,10 +99,16 @@ public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
         Object notification = msg.getObject();
 
         if (notification instanceof RemovalNotification) {
-            System.out.println("To powiadomienie o usunieciu elemnetu");
+            System.out.println("To powiadomienie o usunieciu elemnetu ("
+                    + ((RemovalNotification) notification).key
+                    + ")");
             this.hashTable.remove(((RemovalNotification) notification).key);
         } else if (notification instanceof PutNotification) {
-            System.out.println("To powiadomienie o dodaniu elementu");
+            System.out.println("To powiadomienie o dodaniu elementu ("
+                    + ((PutNotification) notification).key
+                    + " -> "
+                    + ((PutNotification) notification).value
+                    + ")");
             this.hashTable.put(((PutNotification) notification).key, ((PutNotification) notification).value);
         }
     }
@@ -98,15 +135,12 @@ public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
         System.out.println("Ustalilem poczatkowy stan");
     }
 
-
     @Override
     public void viewAccepted(View view) {
         if (view instanceof MergeView) {
-            // determining primary partition
-            // run in a separate thread
+            // wybranie w osobnym watku nowej primary partition po merge'u
             MergeViewHandler handler = new MergeViewHandler(this.communicationChannel, (MergeView) view);
             handler.start();
-
         }
     }
 
@@ -122,30 +156,34 @@ public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
 
         public void run() {
             List<View> subgroups = view.getSubgroups();
-                // picks the first
-                // View primaryPartitionView = subgroups.get(0);
 
-            // picks the one with most nodes
+            // wybranie jako primary partiotion grupy najliczniejszej
             View primaryPartitionView = subgroups.get(0);
-            for(View view : subgroups) {
-                if(view.size() > primaryPartitionView.size())
+            for (View view : subgroups) {
+                if (view.size() > primaryPartitionView.size())
                     primaryPartitionView = view;
             }
 
             Address localAddress = this.communicationChannel.getAddress();
 
             if (!primaryPartitionView.getMembers().contains(localAddress)) {
-                System.out.println("Nie jestes czlonkiem primary partition (" + primaryPartitionView + "), ustalisz stan od nowa");
+                System.out.println("Nie jestes czlonkiem primary partition ("
+                        + primaryPartitionView
+                        + "), ustalisz stan od nowa");
                 try {
                     this.communicationChannel.getState(null, 0);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             } else {
-                System.out.println("Jestes czlonkiem primary partition (" + primaryPartitionView + "), nic nie zrobisz");
+                System.out.println("Jestes czlonkiem primary partition ("
+                        + primaryPartitionView
+                        + "), nic nie zrobisz");
             }
         }
     }
 
-
+    public void close() {
+        this.communicationChannel.close();
+    }
 }
--- lab2/DistributedHashMap/src/main/java/Main.java
+++ lab2/DistributedHashMap/src/main/java/Main.java
@@ -1,91 +1,55 @@
-import org.jgroups.JChannel;
-import org.jgroups.protocols.*;
-import org.jgroups.protocols.pbcast.GMS;
-import org.jgroups.protocols.pbcast.NAKACK2;
-import org.jgroups.protocols.pbcast.STABLE;
-import org.jgroups.stack.ProtocolStack;
-
-import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.setProperty("java.net.preferIPv4Stack", "true");
-
         System.out.println("Start");
 
-        try {
+        DistributedMap distributedMap = new DistributedMap();
 
-            JChannel communicationChannel = new JChannel(false);
+        Scanner scanner = new Scanner(System.in);
+        String operation = "";
 
-            ProtocolStack stack = new ProtocolStack();
-            communicationChannel.setProtocolStack(stack);
+        while (true) {
+            System.out.println("Wpisz jaka operacje chcesz wykonac lub quit? (containsKey|get|put|remove)");
+            operation = scanner.nextLine();
 
-            stack.addProtocol(new UDP().setValue("mcast_group_addr", InetAddress.getByName("224.1.2.3")))
-                    .addProtocol(new PING())
-                    .addProtocol(new MERGE3())
-                    .addProtocol(new FD_SOCK())
-                    .addProtocol(new FD_ALL().setValue("timeout", 12000).setValue("interval", 3000))
-                    .addProtocol(new VERIFY_SUSPECT())
-                    .addProtocol(new BARRIER())
-                    .addProtocol(new NAKACK2())
-                    .addProtocol(new UNICAST3())
-                    .addProtocol(new STABLE())
-                    .addProtocol(new GMS())
-                    .addProtocol(new UFC())
-                    .addProtocol(new MFC())
-                    .addProtocol(new FRAG2());
-            stack.init();
-
-            communicationChannel.connect("distributed_map_cluster");
-//            communicationChannel.setDiscardOwnMessages(true);
-
-            DistributedMap distributedMap = new DistributedMap(communicationChannel);
-
-
-            Scanner scanner = new Scanner(System.in);
-            String operation = "";
-
-            while (true) {
-                System.out.println("Wpisz jaka operacje chcesz wykonac lub quit? (containsKey|get|put|remove)");
-                operation = scanner.nextLine();
-
-                if (operation.equals("containsKey")) {
-                    System.out.println("Wpisz klucz");
-                    String key = scanner.nextLine();
-                    System.out.println(distributedMap.containsKey(key));
-                } else if (operation.equals("get")) {
-                    System.out.println("Wpisz klucz");
-                    String key = scanner.nextLine();
-                    System.out.println(distributedMap.get(key));
-                } else if (operation.equals("put")) {
-                    System.out.println("Wpisz klucz");
-                    String key = scanner.nextLine();
-                    System.out.println("Wpisz wartosc");
-                    int value = scanner.nextInt();
-                    scanner.nextLine();
-                    distributedMap.put(key, value);
+            if (operation.equals("containsKey")) {
+                System.out.println("Wpisz klucz");
+                String key = scanner.nextLine();
+                System.out.println(distributedMap.containsKey(key));
+            } else if (operation.equals("get")) {
+                System.out.println("Wpisz klucz");
+                String key = scanner.nextLine();
+                System.out.println(distributedMap.get(key));
+            } else if (operation.equals("put")) {
+                System.out.println("Wpisz klucz");
+                String key = scanner.nextLine();
+                System.out.println("Wpisz wartosc");
+                String value = scanner.nextLine();
+                try {
+                    distributedMap.put(key, Integer.parseInt(value));
                     System.out.println("Dodano");
-                } else if (operation.equals("remove")) {
-                    System.out.println("Wpisz klucz");
-                    String key = scanner.nextLine();
-                    distributedMap.remove(key);
-                    System.out.println("Usunieto");
-                } else if (operation.equals("quit")) {
-                    System.out.println("Wylaczanie");
-                    break;
-                } else {
-                    System.out.println("Niepoprawna operacja, sprobuj ponownie");
+                } catch (NumberFormatException e) {
+                    System.out.println("To nie liczba!!");
+                    continue;
                 }
+            } else if (operation.equals("remove")) {
+                System.out.println("Wpisz klucz");
+                String key = scanner.nextLine();
+                distributedMap.remove(key);
+                System.out.println("Usunieto");
+            } else if (operation.equals("quit")) {
+                System.out.println("Wylaczanie");
+                break;
+            } else {
+                System.out.println("Niepoprawna operacja, sprobuj ponownie");
             }
-
-            communicationChannel.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+        distributedMap.close();
 
     }
 
